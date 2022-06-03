86d6cbda3e3f9202ed33306964cadfaa9d655cd1
Davi Dantas <davi.soares02@gmail.com>
2022-06-03 00:12:47 -0300

    ultimas alterações

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyCustomScrollBehavior extends MaterialScrollBehavior {
       };
 }
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
