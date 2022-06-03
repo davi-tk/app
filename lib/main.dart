fdf0a65e49077766aff0b1bf5787bc90ecfdbacb
Davi Dantas <davi.soares02@gmail.com>
2022-06-03 00:11:32 -0300

    finalizei o projeto para entrega

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,15 @@ void main() => {
 
 class Human with ChangeNotifier, DiagnosticableTreeMixin {
   int _genre = 0;
-  double _height = 0;
+  double _height = 120;
   double _waist = 40;
+  int _idade = 0;
+
+  //Get e Set para Idade
+  int get idade => _idade;
+  void setIdade(int idade) {
+    _idade = idade;
+  }
 
   //Get e Set pra Gênero
   int get genre => _genre;
@@ -78,20 +85,47 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Descubra seu corpo"),
-      ),
-      body: Center(
+      body: Container(
+        padding: const EdgeInsets.all(40.0),
         child: Column(
           children: [
-            const GenderSelector(),
+            Container(
+                margin: const EdgeInsets.fromLTRB(0, 10, 0, 50),
+                child: const GenderSelector()),
+            const Text("Circunferência de Cintura:"),
             Slider(
               value: context.watch<Human>().waist,
               onChanged: (waist) => context.read<Human>().setWaist(waist),
               min: 40,
               max: 100,
             ),
-            const Count(),
+            const Cintura(),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: const EdgeInsets.only(top: 50, bottom: 200),
+                width: 200,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: "Insira sua idade"),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ElevatedButton(
+                onPressed: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SecondPage(),
+                    ),
+                  )
+                },
+                child: const Icon(Icons.navigate_next_outlined),
+              ),
+            )
           ],
         ),
       ),
@@ -99,8 +133,92 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class Count extends StatelessWidget {
-  const Count({Key? key}) : super(key: key);
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          const Text('Selecione sua altura'),
+          RotatedBox(
+            quarterTurns: 1,
+            child: SizedBox(
+              width: 500,
+              child: Slider(
+                value: context.watch<Human>().height,
+                onChanged: (height) => context.read<Human>().setHeight(height),
+                label: context.watch<Human>().height.toStringAsFixed(2),
+                divisions: 80,
+                min: 120,
+                max: 200,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () => {
+                  Navigator.pop(context),
+                },
+                child: const Icon(Icons.navigate_before_outlined),
+              ),
+              ElevatedButton(
+                onPressed: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ResultPage(),
+                    ),
+                  ),
+                },
+                child: const Icon(Icons.navigate_next_outlined),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ResultPage extends StatelessWidget {
+  const ResultPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var altura = context.watch<Human>().height;
+    var genero = context.watch<Human>().genre;
+    var cintura = context.watch<Human>().waist;
+
+    var resultado = -20 * (altura / cintura);
+    resultado += genero == 0 ? 76 : 64;
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+                "O Resultado da sua Massa Gorda Relativa é ${resultado.toStringAsFixed(2)}",
+                style: Theme.of(context).textTheme.headline5),
+          ),
+          ElevatedButton(
+            onPressed: () => {
+              Navigator.pop(context),
+            },
+            child: const Icon(Icons.navigate_before_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Cintura extends StatelessWidget {
+  const Cintura({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -122,7 +240,7 @@ class GenderSelector extends StatelessWidget {
       children: <Widget>[
         Expanded(
           child: ListTile(
-            title: const Text('Mulher'),
+            title: const Text('Feminino'),
             leading: Radio<int>(
               value: 0,
               groupValue: context.watch<Human>().genre,
@@ -132,7 +250,7 @@ class GenderSelector extends StatelessWidget {
         ),
         Expanded(
           child: ListTile(
-            title: const Text('Homem'),
+            title: const Text('Masculino'),
             leading: Radio<int>(
               value: 1,
               groupValue: context.watch<Human>().genre,
